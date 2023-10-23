868fe4e02aac6a36f5c2e51e74a78206fcd31be5
himanushi <himanushi@users.noreply.github.com>
2023-10-23 14:52:56 +0900

    画像追加

---
+++ src/textures.ts
@@ -0,0 +1,51 @@
+import * as PIXI from "pixi.js";
+import card0Image from "./assets/cards/0.png";
+import card1Image from "./assets/cards/1.png";
+import card2Image from "./assets/cards/2.png";
+import card3Image from "./assets/cards/3.png";
+import card4Image from "./assets/cards/4.png";
+import card5Image from "./assets/cards/5.png";
+import card6Image from "./assets/cards/6.png";
+import card7Image from "./assets/cards/7.png";
+import card8Image from "./assets/cards/8.png";
+import card9Image from "./assets/cards/9.png";
+import card10Image from "./assets/cards/10.png";
+import card11Image from "./assets/cards/11.png";
+import card12Image from "./assets/cards/12.png";
+import card13Image from "./assets/cards/13.png";
+import card14Image from "./assets/cards/14.png";
+import card15Image from "./assets/cards/15.png";
+import card16Image from "./assets/cards/16.png";
+import card17Image from "./assets/cards/17.png";
+import card18Image from "./assets/cards/18.png";
+import card19Image from "./assets/cards/19.png";
+import card20Image from "./assets/cards/20.png";
+import card21Image from "./assets/cards/21.png";
+import backCardImage from "./assets/cards/back.png";
+
+export const backCardTexture = PIXI.Texture.from(backCardImage);
+
+export const cardTextures = [
+  PIXI.Texture.from(card0Image),
+  PIXI.Texture.from(card1Image),
+  PIXI.Texture.from(card2Image),
+  PIXI.Texture.from(card3Image),
+  PIXI.Texture.from(card4Image),
+  PIXI.Texture.from(card5Image),
+  PIXI.Texture.from(card6Image),
+  PIXI.Texture.from(card7Image),
+  PIXI.Texture.from(card8Image),
+  PIXI.Texture.from(card9Image),
+  PIXI.Texture.from(card10Image),
+  PIXI.Texture.from(card11Image),
+  PIXI.Texture.from(card12Image),
+  PIXI.Texture.from(card13Image),
+  PIXI.Texture.from(card14Image),
+  PIXI.Texture.from(card15Image),
+  PIXI.Texture.from(card16Image),
+  PIXI.Texture.from(card17Image),
+  PIXI.Texture.from(card18Image),
+  PIXI.Texture.from(card19Image),
+  PIXI.Texture.from(card20Image),
+  PIXI.Texture.from(card21Image),
+];
