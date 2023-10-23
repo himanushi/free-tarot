ced49737a01ce6276c8a785d768fde95a3dbd208
himanushi <himanushi@users.noreply.github.com>
2023-10-23 14:53:10 +0900

    remove react

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import * as PIXI from "pixi.js";
-
-// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-const app: any = new PIXI.Application({
-  background: "#1099bb",
-  resizeTo: window,
-});
-
-document.body.appendChild(app.view);
